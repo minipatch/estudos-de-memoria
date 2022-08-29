4009e1a9014a441bef09b8065639efad3672b99a
minipatch <lucao322@gmail.com>
2022-08-29 18:20:15 -0300

    vpl1 commit

---
+++ vpl1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+    std::string frase;
+    std::cin>>frase;
+    std::string vogais = "a,e,i,o,u";
+    for(int i=0;i<frase.size();i++){
+        //verificar se ha alguma vogal na frase
+        if(frase == 'a'){
+           int j;
+           j++; 
+        }
+    }
+}
