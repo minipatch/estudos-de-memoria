c62e2bfb63a9b350d60358cd84df0e2830689520
minipatch <lucao322@gmail.com>
2022-09-14 10:25:49 -0300

    vpl3praentregar

--- vpl3.cpp
+++ vpl3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <map>
+#include <vector>
+#include <ostream>
 
 class fatura{
     private:
@@ -10,8 +12,10 @@ class fatura{
         double valor;
     
     public:
-    
-        fatura(std::string _codigo,char _tpFatura, _double _valor){
+        fatura(){
+
+        }
+        fatura(std::string _codigo,char _tpFatura, double _valor){
             this->codigo=_codigo;
             this->tpFatura=_tpFatura;
             this->valor=_valor;
@@ -33,29 +37,63 @@ class fatura{
 
 class colecao{
     private:    
-        std::list<int> faturas;
+        std::map<char,int> status;
+        std::vector<fatura> lista;
     
     public:
         colecao(){
-            this->faturas=0;
+            
         }
 
         void adicionar(fatura f){
-
+            lista.push_back(f);
+            for(int i=0;i<lista.size();i++){
+                status[i];
+            }
         }
 
         void proxima(){
-
+            lista.pop_back();
+            imprimir();
+            for(int i=lista.size();0<i;i--){
+                status[i];
+            }
         }
 
         void imprimir(){
-
+        //preciso de help
+        //std::cout<<lista<<std::endl;                
+            
         }
 
         void status(){
-            
+            for(auto it:status){
+                std::cout<<it.first<<std::endl<<it.second<<std::endl;
+            }
         }
 
 
 
 };
+
+
+int main(){
+    const char f='f',r='r',p='p',s='s',e='e';
+    char letra;
+    std::cin>>letra;
+    
+    if(letra==f){
+        std::string cod;
+        char tpFatura;
+        double valor; 
+        std::cin>>cod>>tpFatura>>valor;
+        fatura Fatura(cod,tpFatura,valor);
+    }
+    if(letra==r){
+        Fatura.proxima();
+    }
+
+    if(letra==p){
+        
+    }
+}
