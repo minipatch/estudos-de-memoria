8c6f381d8fc9468617b2a8b2c01118e634635a31
minipatch <lucao322@gmail.com>
2022-10-06 11:06:32 -0300

    vplgrandeinciado

---
+++ vpl6/src/produto.cpp
@@ -0,0 +1,34 @@
+// TODO implemente essa classe de acordo com o hpp correspondente
+#include <iostream>
+#include "include/produto.hpp"
+
+Produto::Produto(int qtdprod,float preco,std::string valor){
+    this->_quantidade=qtdprod;
+    this->_valor_unitario=preco;
+
+}
+
+int Produto::getQtd()const{
+    return _quantidade;
+}
+
+float Produto::getValor()const{
+    return _valor_unitario;
+}
+
+std::string Produto::descricao()const {
+    std::string descricao_do_produto;
+    return descricao_do_produto;
+}
+
+float Produto::calcPreco(){
+    float soma;
+    for(int i=0;i<getQtd();i++){
+        soma=+_valor_unitario;
+    }
+    return soma;
+}
+
+Produto::~Produto(){}
+
+
