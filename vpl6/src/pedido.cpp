58cbcff754c1e1cd0baa4568fc30eeccbccfd0a1
minipatch <lucao322@gmail.com>
2022-10-10 19:34:43 -0300

    gitdealgumas alterações

--- vpl6/src/pedido.cpp
+++ vpl6/src/pedido.cpp
@@ -5,36 +5,15 @@
 #include "include/cachorro_quente.hpp"
 #include "include/produto.hpp"
 
-Pedido::Pedido(std::list<Produto*> produtos,
+Pedido::Pedido(std::list<Produto* > produtos,
 std::list<Acai*> Acai,
-std::list<CachorroQuente*>Cachorro,
-std::list<Pizza*>Pizza):
-_Produtos(produtos),_Acai(Acai),_Cachorro(Cachorro),
-_Pizza(Pizza){}
+std::list<CachorroQuente*> Cachorro, 
+std::list<Pizza*> Pizza) : _Produtos(produtos),
+_Acai(Acai), _Cachorro(Cachorro), _Pizza(Pizza){}
 
-//a revisar
+Pedido::~Pedido(){}
+
 void Pedido::adicionaProduto(Produto* p){
-    _Produtos.push_back(p);
+
 }
 
-// a revisar
-float Pedido::calculaTotal() const{
-    float preco=1;
-    for (int i= 0 ; i < _Produtos.size();i++){
-        preco++;
-    }
-    return preco;
-}
-// a revisar
-std::string Pedido::resumo() const{
-    std::string Resumo;
-    if(!_Produtos.empty()){
-        for(int i=0;i<_Produtos.size();i++){
-            
-        }
-    }
-}
-
-
-
-    
